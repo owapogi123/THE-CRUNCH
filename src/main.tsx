62cdc0018010a146bdf9e680fd7ab84ba4f839ee
jaycii0007 <carlbalderamos@gmail.com>
2026-04-03 12:39:41 +0800

    fix the blank page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react'
 import "./styles/globals.css";
 import App from './App.tsx'
+import { NotificationProvider } from "./lib/NotificationContext";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </StrictMode>
   </BrowserRouter>
 )
